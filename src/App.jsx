e547232b7501b6dee10143771ead9833bd6077b0
kelliwisethebrave <198285550+kelliwisethebrave@users.noreply.github.com>
2026-04-08 21:20:24 -0600

    Update to Coming Soon status with initial inventory added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
 import ctdLogo from "./assets/mono-blue-logo.svg";
 import "./App.css";
+import inventoryData from "./assets/inventory.json";
+import { useState } from "react";
 
 function App() {
+  const [inventory, setInventory] = useState(inventoryData.inventory);
+
   return (
-    <div className="coming-soon">
-      <h1>CTD Swag</h1>
-      <div style={{ height: 100, width: 100 }}>
-        <img src={ctdLogo} alt="Code The Dream Logo" />
+    <main>
+      <div className="coming-soon">
+        <h1>CTD Swag</h1>
+        <div style={{ height: 100, width: 100 }}>
+          <img src={ctdLogo} alt="Code The Dream Logo" />
+        </div>
+        <h2>Coming Soon...</h2>
       </div>
-      <h2>Coming Soon...</h2>
-    </div>
+      <ul>
+        {inventory.map((item) => {
+          return (
+            <li key={item.id}>
+              <div className="itemCard">
+                <h2>{item.baseName}</h2>
+                <p>{item.baseDescription}</p>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </main>
   );
 }
 
